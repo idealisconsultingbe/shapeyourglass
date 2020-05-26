7771b23f7acddad4f935d909dcc2ef622cede247
Pierrick Fichefet <pfi@idealisconsulting.com>
2020-05-26 22:03:56 +0200

    [AS-37] Wizard PF views init2

--- agc_manufacturing/models/agc_sale_order_line.py
+++ agc_manufacturing/models/agc_sale_order_line.py
@@ -9,5 +9,22 @@ class AGCSaleOrderLine(models.Model):
 
     product_manufacture_spec_ids = fields.One2many('product.manufacture.specification', 'sale_line_id', string='Finished Product Specifications')
     finished_product_unit_cost = fields.Monetary(string='Finished Product Unit Cost', default=0.0)
-    finished_product_quantity = fields.Integer(string='Quantity', default=0)
+    finished_product_quantity = fields.Integer(string='Mothersheet / DLS', default=0)
     configuration_is_done = fields.Boolean(string='FP Configuration is done', default=False, help='Technical field that helps to know whether the Finished Product configuration is done.')
+
+    def open_fp_configuration_view(self):
+        """
+        Return a different view depending on whether the SO is confirmed.
+        """
+        self.ensure_one()
+        if self.state not in ('sale', 'done'):
+            view_id = self.env.ref('agc_manufacturing.sale_order_line_form_product_manufacture_spec_view').id
+        else:
+            view_id = self.env.ref('agc_manufacturing.sale_order_line_confirmed_form_product_manufacture_spec_view').id
+        return {
+            'type': 'ir.actions.act_window',
+            'view_mode': 'form',
+            'res_model': 'sale.order.line',
+            'view_id': view_id,
+            'target': 'new',
+        }
