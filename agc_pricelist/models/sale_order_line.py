6b6bb923b926e273f958be51f1fe0fd26adf62de
DWA <dwa@idealisconsulting.com>
2020-10-05 11:25:48 +0200

    [FIX] prevent onchange computation

--- agc_pricelist/models/sale_order_line.py
+++ agc_pricelist/models/sale_order_line.py
@@ -13,7 +13,7 @@ class SaleOrderLine(models.Model):
         Use line cost in context to define price unit of a sale order line
         """
         if 'purchase_price' in values:
-            # self.order.partner_id and self.product_id are mandatory
+            # self.order_id.partner_id and self.product_id are mandatory
             if self.order_id.pricelist_id and self.order_id.pricelist_id.discount_policy == 'with_discount':
                 final_price, rule_id = self.order_id.pricelist_id.with_context(line_cost=values['purchase_price']).get_product_price_rule(self.product_id, self.product_uom_qty or 1.0, self.order_id.partner_id)
                 values['price_unit'] = final_price
@@ -22,7 +22,7 @@ class SaleOrderLine(models.Model):
     @api.onchange('purchase_price')
     def _onchange_purchase_price(self):
         """ If purchase price changes, update price unit accordingly """
-        # self.order.partner_id and self.product_id are mandatory
-        if self.order_id.pricelist_id and self.order_id.pricelist_id.discount_policy == 'with_discount':
+        # prevent computation even if there is no partner and no product
+        if self.order_id.pricelist_id and self.order_id.pricelist_id.discount_policy == 'with_discount' and self.order_id.partner_id and self.product_id:
             final_price, rule_id = self.order_id.pricelist_id.with_context(line_cost=self.purchase_price).get_product_price_rule(self.product_id, self.product_uom_qty or 1.0, self.order_id.partner_id)
             self.update({'price_unit': final_price})
